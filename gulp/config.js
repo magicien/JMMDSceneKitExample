4ade8de822ea223ef65fb1fbefed0f1436e724dd
magicien <magicien.du.ballon@gmail.com>
2017-10-16 13:44:46 -0700

    Update packages

--- gulp/config.js
+++ gulp/config.js
@@ -72,7 +72,7 @@ module.exports = {
             exclude: /node_modules/,
             loader: 'babel-loader',
             query: {
-              presets: ['es2015']
+              presets: ['env']
             }
           }
         ]
